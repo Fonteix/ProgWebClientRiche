24707d742753bf8051b155a84e9e0f2889469b50
Sinhnyphoy <sinhnyphoy.sisouphanthong@gmail.com>
2017-10-21 00:19:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function main() {
     document.getElementById('pause').addEventListener('click', pause);
     document.getElementById('next').addEventListener('click', next);
     document.getElementById('previous').addEventListener('click', previous);
+    document.getElementById('mute/unmute').addEventListener('click', mute);
+	document.getElementById('volUp').addEventListener('click', volUp);
+	document.getElementById('volDown').addEventListener('click', volDown);
 }
 
 function play() {
@@ -21,4 +24,31 @@ function next() {
 
 function previous() {
     console.log("previous");
+}
+function volUp() {
+	alterVolume('+');
+}
+
+function volDown() {
+	alterVolume('-');
+}
+
+var alterVolume = function(dir) {
+	var video = document.getElementById('video1');
+	var currentVolume = Math.floor(video.volume * 10)/10;
+	if(dir === '+') {
+		if(currentVolume<1) {
+			video.volume += 0.1;
+		}
+	}
+	else if(dir === '-') {
+		if(currentVolume>0) {
+			video.volume -= 0.1;
+		}
+	}
+}
+
+function mute() {
+	var video = document.getElementById('video1');
+	video.muted = !video.muted;
 }
